c3984679c79f253247a13c4bed4393100ab97e91
Antra <79895853+antra-gaile@users.noreply.github.com>
2021-04-29 13:22:47 +0300

    Update update_210429_A.py

--- update_210429_A.py
+++ update_210429_A.py
@@ -16,34 +16,23 @@ subsection = ["Electron_pt","nElectron","Electron_mass","Electron_eta","Electron
 ds = tree.pandas.df(subsection, entrystop=cik)
 print("I have the branches, {0:.3f} s".format(time.time()-start))
 
-el_pt1,el_pt2 =[], []
-el_eta1,el_eta2 =[], []
-el_mass1,el_mass2 =[], []
-el_p1,el_p2,el_P =[],[], []
-el_E1,el_E2,el_E =[],[], []
+el_E =[]
 
 df=ds.query("nElectron==2")
 df=df.reset_index()
-print("I have the diE, {0:.3f} s".format(time.time()-start))
+print("I have the diE, {1}, {0:.3f} s".format(time.time()-start),len(df["nElectron"]))
 
 for i in range(len(df["nElectron"])):
-    if df.query("nElectron==2")["Electron_charge"][i][0]+df.query("nElectron==2")["Electron_charge"][i][1] == 0:
-        el_pt1.append(df.query("nElectron==2")["Electron_pt"][i][0])
-        el_pt2.append(df.query("nElectron==2")["Electron_pt"][i][1])
-        el_eta1.append(df.query("nElectron==2")["Electron_eta"][i][0])
-        el_eta2.append(df.query("nElectron==2")["Electron_eta"][i][1])
-        el_mass1.append(df.query("nElectron==2")["Electron_mass"][i][0])
-        el_mass2.append(df.query("nElectron==2")["Electron_mass"][i][1])
-
+    if df["Electron_charge"][i][0]+df["Electron_charge"][i][1] == 0:
+        el_p1=df["Electron_pt"][i][0]/np.cos(np.arctan(np.exp(-df["Electron_eta"][i][0])))
+        el_p2=df["Electron_pt"][i][1]/np.cos(np.arctan(np.exp(-df["Electron_eta"][i][1])))
+        el_E1=(df["Electron_mass"][i][0]**2 + el_p1**2)**0.5
+        el_E2=(df["Electron_mass"][i][1]**2 + el_p2**2)**0.5
+        el_E.append(el_E1+el_E2)    
+        
 print("I have the raw data, {0:.3} s".format(time.time()-start))
-for i in range(len(el_pt1)):
-    el_p1.append(el_pt1[i]/np.cos(np.arctan(np.exp(-el_eta1[i]))))
-    el_p2.append(el_pt2[i]/np.cos(np.arctan(np.exp(-el_eta2[i]))))
-    el_E1.append((el_mass1[i]**2 + el_p1[i]**2)**0.5)
-    el_E2.append((el_mass2[i]**2 + el_p2[i]**2)**0.5)
-    el_E.append(el_E1[i]+el_E2[i])
 
-print("I shall start to draw, {0:.3f} s".format(time.time()-start))
+#print("I shall start to draw, {0:.3f} s".format(time.time()-start))
 plt.hist(el_E,bins=180,range=[0,180])
 plt.xlabel("2_E, GeV")
 plt.ylabel("Frequency")
