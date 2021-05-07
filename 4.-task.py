af1e6bbf71c2f155fc03435d11af9b4c86a766d8
Normunds Strautnieks <79871354+pauris123@users.noreply.github.com>
2021-05-07 16:05:58 +0300

    Create 4.-task.py

---
+++ 4.-task.py
@@ -0,0 +1,126 @@
+import ROOT
+import time
+start=time.time()
+import uproot
+import matplotlib.pyplot as plt
+import numpy as np
+import math as math
+
+print("I have the libraries")
+
+rootfile="root://cmsxrootd.fnal.gov///store/mc/RunIISummer16NanoAODv7/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/NANOAODSIM/PUMoriond17_Nano02Apr2020_plus5percentMaterial_102X_mcRun2_asymptotic_v8-v1/120000/07B01D04-E364-7B43-B52B-EBB19A971F8C.root"
+
+tree = uproot.open(rootfile)["Events"]
+
+print("I have the tree, {0:.3f} s".format(time.time()-start))
+
+cik=300000
+subsection = ["Electron_pt","nElectron","Electron_eta","Electron_charge","Electron_phi"]
+ds = tree.pandas.df(subsection, entrystop=cik) # We take relevant data
+
+print("I have the branches, {0:.3f} s".format(time.time()-start))
+
+el_inv = []
+repetition = []
+el_mass = 0.000511
+Z_mass = []
+
+df=ds.query("nElectron==3") # We choose only 3 electron events
+df=df.reset_index() # Reindex it
+
+print("I have the triE, {1}, {0:.3f} s".format(time.time()-start,len(df["nElectron"])))
+
+for i in range(len(df["nElectron"])):
+    if df["Electron_charge"][i][0]+df["Electron_charge"][i][1] == 0: # 1.-2. electron pair analysis
+
+        el1_pt = df["Electron_pt"][i][0]
+        el1_eta = df["Electron_eta"][i][0]
+        el1_phi = df["Electron_phi"][i][0]
+        el2_pt = df["Electron_pt"][i][1]
+        el2_eta = df["Electron_eta"][i][1]
+        el2_phi = df["Electron_phi"][i][1]
+        
+        elec1 = ROOT.TLorentzVector()
+        elec2 = ROOT.TLorentzVector()
+        elec1.SetPtEtaPhiM(el1_pt, el1_eta, el1_phi, el_mass)
+        elec2.SetPtEtaPhiM(el2_pt, el2_eta, el2_phi, el_mass)
+        mass = (elec1+elec2).M()
+        if mass > 70 and mass < 110:
+            el_inv.append(mass)
+            repetition.append(i)
+        
+        #invariant2 = 2*el1_pt*el2_pt*(math.cosh(el1_eta-el2_eta)-math.cos(el1_phi-el2_phi))
+        #if (invariant2)**0.5 > 70 and (invariant2)**0.5 < 110:
+            #Z_mass.append((invariant2)**0.5)
+    
+    if df["Electron_charge"][i][0]+df["Electron_charge"][i][2] == 0: # 1.-3. electron pair analysis
+
+        el1_pt = df["Electron_pt"][i][0]
+        el1_eta = df["Electron_eta"][i][0]
+        el1_phi = df["Electron_phi"][i][0]
+        el2_pt = df["Electron_pt"][i][2]
+        el2_eta = df["Electron_eta"][i][2]
+        el2_phi = df["Electron_phi"][i][2]
+        
+        elec1 = ROOT.TLorentzVector()
+        elec2 = ROOT.TLorentzVector()
+        elec1.SetPtEtaPhiM(el1_pt, el1_eta, el1_phi, el_mass)
+        elec2.SetPtEtaPhiM(el2_pt, el2_eta, el2_phi, el_mass)
+        mass = (elec1+elec2).M()
+        if mass > 70 and mass < 110:
+            el_inv.append(mass)
+            repetition.append(i)
+        
+        #invariant2 = 2*el1_pt*el2_pt*(math.cosh(el1_eta-el2_eta)-math.cos(el1_phi-el2_phi))
+        #if (invariant2)**0.5 > 70 and (invariant2)**0.5 < 110:
+            #Z_mass.append((invariant2)**0.5)
+    
+    if df["Electron_charge"][i][1]+df["Electron_charge"][i][2] == 0: # 2.-3. electron pair analysis
+        
+        
+        el1_pt = df["Electron_pt"][i][1]
+        el1_eta = df["Electron_eta"][i][1]
+        el1_phi = df["Electron_phi"][i][1]
+        el2_pt = df["Electron_pt"][i][2]
+        el2_eta = df["Electron_eta"][i][2]
+        el2_phi = df["Electron_phi"][i][2]
+        
+        elec1 = ROOT.TLorentzVector()
+        elec2 = ROOT.TLorentzVector()
+        elec1.SetPtEtaPhiM(el1_pt, el1_eta, el1_phi, el_mass)
+        elec2.SetPtEtaPhiM(el2_pt, el2_eta, el2_phi, el_mass)
+        mass = (elec1+elec2).M()
+        if mass > 70 and mass < 110:
+            el_inv.append(mass)
+            repetition.append(i)
+        
+        #invariant2 = 2*el1_pt*el2_pt*(math.cosh(el1_eta-el2_eta)-math.cos(el1_phi-el2_phi))
+        #if (invariant2)**0.5 > 70 and (invariant2)**0.5 < 110:
+            #Z_mass.append((invariant2)**0.5)
+              
+print("I have the raw data, {0:.3} s".format(time.time()-start))
+
+repeat = []
+for i in range(len(repetition)-1): # We check how many data points use the same data entry twice
+    if repetition[i] == repetition[i+1]:
+        repeat.append(repetition[i])
+        
+print("We used same event twice " + str(len(repeat)) + " times... We need to fix this somehow")
+
+
+#plt.figure(1)
+#plt.hist(Z_mass,bins=180,range=[0,180], alpha=0.7, histtype=u'step')
+#plt.xlabel("M, GeV")
+#plt.ylabel("Frequency")
+#plt.title("Tonis given NANOAOD with 'Formula', {0} entries, t={1:.0f} s".format(cik,time.time()-start))
+#plt.xticks(np.arange(0,190,10))
+
+#plt.figure(2)
+plt.hist(el_inv,bins=180,range=[0,180], alpha=0.7, histtype=u'step')
+plt.xlabel("M, GeV")
+plt.ylabel("Frequency")
+plt.title("Tonis given NANOAOD with 'Function', {0} entries, t={1:.0f} s".format(cik,time.time()-start))
+plt.xticks(np.arange(0,190,10))
+print("Ready to show, {0:.3f} s".format(time.time()-start))
+#plt.savefig("{0}_pilnP.png".format(cik))
+plt.show()
