e9bce20a3b09eba99547539990d7649ade9bcd2d
Antra <79895853+antra-gaile@users.noreply.github.com>
2021-06-15 17:19:26 +0300

    Update tool_draw_a_hist.py
    Pievienoju, lai zīmē gan data, gan mc, pašreiz svari visiem vienādi.

--- tool_draw_a_hist.py
+++ tool_draw_a_hist.py
@@ -3,13 +3,24 @@ import pandas as pn
 import matplotlib.pyplot as plt
 
 bi=180
-file="2017_data.txt"
-df=pn.read_csv(file, sep='\t',names=["mass"])
+koef=2.33
+file_d="2017_UL_data_1.txt"
+file_mc="2017_UL_mc.txt"
+df_d=pn.read_csv(file_d, sep='\t',names=["mass"])
+df_mc=pn.read_csv(file_mc, sep='\t',names=["mass"])
 
-plt.hist(df["mass"],bins=bi,range=[0,180], alpha=0.7, histtype=u'step')
+y, edges = np.histogram(df_d["mass"], bins=bi, range=(0, 180))
+x = (edges[1:] + edges[:-1])/2
+
+plt.hist(df_mc["mass"],bins=bi,range=[0,180], alpha=0.7,
+                 histtype=u'step',weights=np.ones(len(df_mc["mass"]))*koef,label="MC")
+plt.errorbar(x,y,yerr=yerr=np.sqrt(y),fmt="o",capsize=3,label="Data")
 plt.xlabel("2_E, GeV")
 plt.ylabel("Frequency")
-plt.title("2017, data")
+plt.title("y2017, koef={0}".format(koef))
 plt.xticks(np.arange(0,190,10))
-plt.savefig("{0}.png".format(file[:-4]))
+plt.ylim([0,4000000])
+plt.xlim([55,125])
+plt.legend(loc="best")
+plt.savefig("d_mc_combo_{0}.png".format(koef))
 plt.show()
