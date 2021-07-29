a855f68c0032ad3a13af2f0a57fe9602c052601d
Antra <79895853+antra-gaile@users.noreply.github.com>
2021-07-29 11:28:27 +0300

    Update tool_fit_n_ratio_plot.py
    strīpiņa

--- tool_fit_n_ratio_plot.py
+++ tool_fit_n_ratio_plot.py
@@ -15,13 +15,13 @@ print("{0}\tYea, I have the libraries".format(l_wall_time(time.time()-start)))
 ### will try out the Gauss
 ### not sure about the double-sided part :)
 #f = lambda x, a, b, c: a*st.norm.pdf(x,b,c)
-f = lambda x, a, b, c,d,e,g: a*st.norm.pdf(x,b,c)+d*st.norm.pdf(x,e,g)
+f = lambda x, a, b, c, d, g: a*st.norm.pdf(x,b,c)+d*st.norm.pdf(x,b,g)
 ### the background is assumed linear
 fb = lambda x, a, b: a*x+b
 
 no,lidz=55,125
 bi=lidz-no
-file_d="2017_UL_data_1.txt"
+file_d="2017_2_data.txt"
 file_mc="2017_MC_dimass_n_weights_0_72.txt"
 df_mc=pn.read_csv(file_mc, sep='\t',names=["mass","weights"])
 df_d=pn.read_csv(file_d, sep='\t',names=["mass"])
@@ -43,7 +43,7 @@ print("{0}\tFound the bckg".format(l_wall_time(time.time()-start)))
 ### to extract signal: 
 s_d_y=y_d-fb(x_d,*parametersb_d)
 s_d_x=x_d 
-parameters_d, err_d = opt.curve_fit(f, s_d_x, s_d_y, p0=(30000000,90,4,10000,90,10)) 
+parameters_d, err_d = opt.curve_fit(f, s_d_x, s_d_y, p0=(30000000,90,4,10000,10)) 
 print("{0}\tAnd extracted the signal".format(l_wall_time(time.time()-start)))
 
 ### for MC we must include weights
@@ -62,7 +62,7 @@ print("{0}\tFound the bckg".format(l_wall_time(time.time()-start)))
 ### signal in mc
 s_mc_y=y_mc-fb(x_mc,*parametersb_mc)
 s_mc_x=x_mc 
-parameters_mc, err_mc = opt.curve_fit(f, s_mc_x, s_mc_y, p0=(30000000,90,4,10000,90,10)) 
+parameters_mc, err_mc = opt.curve_fit(f, s_mc_x, s_mc_y, p0=(30000000,90,4,10000,10)) 
 print("{0}\tAnd extracted the signal".format(l_wall_time(time.time()-start)))
 
 x_fit=np.linspace(no,lidz,100)
@@ -70,16 +70,16 @@ d_v_mc=s_d_y/s_mc_y
 
 print("{0}\toh, diddly, here it comes!".format(l_wall_time(time.time()-start)))
 ### other decorations
-fig, ax = plt.subplots(2,1, figsize=(8,8), gridspec_kw={'height_ratios': [3, 1]})
+fig, ax = plt.subplots(2,1, figsize=(8,12), gridspec_kw={'height_ratios': [3, 1]})
 #fig.suptitle('y2017, 2Gauss fit, with PUweights, without smearing and SF', fontsize=18)
 
 ax[0].errorbar(s_d_x,s_d_y,yerr_d,fmt="k.", markerfacecolor='none',capsize=3, label="signal in data") 
-ax[0].plot(x_fit,f(x_fit,*parameters_d),label="data fit with 2G") 
+ax[0].plot(x_fit,f(x_fit,*parameters_d),label="data fit with 2G, peak = {0:.2f} GeV".format(parameters_d[1])) 
 
 ax[0].bar(s_mc_x,s_mc_y,color="k", alpha=0.1, label="signal in mc")
-ax[0].plot(x_fit,f(x_fit,*parameters_mc),label="2G MC") 
+ax[0].plot(x_fit,f(x_fit,*parameters_mc),label="MC fit with 2G, peak = {0:.2f} GeV".format(parameters_mc[1])) 
 
-ax[0].legend(loc='best')
+ax[0].legend(loc='best', bbox_to_anchor=(1, 0.5))
 ax[0].set_xlabel("dielectron mass, GeV")
 ax[0].set_ylabel("Frequency")
 ax[0].set_ylim([0,4000000])
@@ -87,10 +87,13 @@ ax[0].set_xlim([no,lidz])
 ax[0].set_title('y2017, 2Gauss fit, with PUweights, without smearing and SF')
 
 ax[1].scatter(s_d_x,d_v_mc,color="k",marker=".", facecolors='none',label="data/MC")
-ax[1].legend(loc='best')
+ax[1].plot(s_d_x,np.ones(len(s_d_x)),lw=1,color="k",alpha=0.7)
+ax[1].plot(s_d_x,np.ones(len(s_d_x)),lw=1,color="r",linestyle="--",alpha=0.7)
 ax[1].set_xlabel("dielectron mass, GeV")
 ax[1].set_ylabel("Data/MC")
 ax[1].set_xlim([no,lidz])
+ax[1].set_ylim([-2,3])
 
-plt.savefig("d_mc_combo_fit_wpu_2gauss_ratiopl.png",bbox_inches = "tight") 
+
+plt.savefig("d_mc_combo_fit_wpu_2gauss_ratiopl_updated_data.png",bbox_inches = "tight") 
 plt.show()
