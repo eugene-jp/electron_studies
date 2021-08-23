1b50f15eec537ff20ffcd060360143b9a9c05b36
Antra <79895853+antra-gaile@users.noreply.github.com>
2021-08-23 20:34:36 +0300

    Update Z_from_2_electrons+weights.py
    SF from file

--- Z_from_2_electrons+weights.py
+++ Z_from_2_electrons+weights.py
@@ -14,12 +14,13 @@ def l_wall_time(seconds):
     m, s = divmod(seconds, 60)
     return "{0:.0f}min:{1:.0f}s".format(m,s)
 
-names=pn.read_csv("nosaukumi_2017_MC.txt", sep='\t',names=["nos"]) 
+names=pn.read_csv("nosaukumi_2017_MC_v9.txt", sep='\t',names=["nos"]) 
 PUweights= uproot.open("pu_weights_2017.root")["weights"].values
+sf=pn.read_csv("2017_mva90_iso_quick.txt",sep="\t") #made it with the tool here in github, or find it in my public folder
 print("I have the libraries and names")
 
 notik=0
-tik=72 #len(names["nos"])
+tik=len(names["nos"])
 for aiziet in range(notik,tik):
     print("\n {0}/{1}, {2}".format(aiziet+1,tik,wall_time(time.time()-bigBang)))
     start=time.time()
@@ -30,7 +31,9 @@ for aiziet in range(notik,tik):
 
     el_mass = []
     el_weight = []
-    subsection = ["Electron_pt","nElectron","Electron_eta","Electron_charge","Electron_phi","Pileup_nPU"]
+    subsection = ["Electron_pt","nElectron","Electron_eta",
+                  "Electron_charge","Electron_phi","Pileup_nPU",
+                  "Electron_mvaFall17V2Iso_WP90"]
     ds = tree.pandas.df(subsection, entrystop=-1)
     print(" {0}\tI have the branches".format(l_wall_time(time.time()-start)))
 
@@ -39,7 +42,7 @@ for aiziet in range(notik,tik):
     print(" {0}\tI have the diE, {1}".format(l_wall_time(time.time()-start),len(df["nElectron"])))
 
     for i in range(len(df["nElectron"])):
-        if df["Electron_charge"][i][0]+df["Electron_charge"][i][1] == 0:
+        if (df["Electron_charge"][i][0]+df["Electron_charge"][i][1] == 0) and ((df["Electron_mvaFall17V2Iso_WP90"][i][0] and df["Electron_mvaFall17V2Iso_WP90"][i][0])== True):
             el1 = ROOT.TLorentzVector()
             el2 = ROOT.TLorentzVector()
 
@@ -51,7 +54,16 @@ for aiziet in range(notik,tik):
                              df["Electron_phi"][i][1],0.000511)
             el_mass.append((el1+el2).M())   
             
-            weights = 41.48 * 5765400 * 1 * 1 * PUweights[df["Pileup_nPU"][i]]/102486448
+            sf1,sf2=0,0 
+            for mekle in range(len(sf)):
+                if df["Electron_eta"][i][0]>sf["pseido_no"][mekle] and df["Electron_eta"][i][0]<sf["pseido_lidz"][mekle]:
+                    if df["Electron_pt"][i][0]>sf["pt_no"][mekle] and df["Electron_pt"][i][0]<sf["pt_lidz"][mekle]:
+                        sf1=sf["SF"][mekle]
+                if df["Electron_eta"][i][1]>sf["pseido_no"][mekle] and df["Electron_eta"][i][1]<sf["pseido_lidz"][mekle]:
+                    if df["Electron_pt"][i][1]>sf["pt_no"][mekle] and df["Electron_pt"][i][1]<sf["pt_lidz"][mekle]:
+                        sf2=sf["SF"][mekle]
+            
+            weights = 41.48 * 5765400 * 1 * sf1 * sf2 * PUweights[df["Pileup_nPU"][i]]/102486448
             el_weight.append(weights)
 
     filetime=time.time()-start
@@ -60,12 +72,12 @@ for aiziet in range(notik,tik):
     #print("\nShall save")
     savetime=time.time()
     
-    fo=open("2017_MC_dimass_n_weights_{0}_{1}.txt".format(notik,tik),"a")
+    fo=open("2017_MC_v9_dimass_n_weights_{0}_{1}.txt".format(notik,tik),"a")
     for i in range(len(el_mass)):
         fo.write(str(el_mass[i])+"\t"+str(el_weight[i])+"\n")
     fo.close()
     
-    fs=open("2017_MC_progress_{0}_{1}.txt".format(notik,tik),"a")
+    fs=open("2017_MC_v9_progress_{0}_{1}.txt".format(notik,tik),"a")
     fs.write("{0}\t{1}\t{2}\t{3:.2f}\n".format(aiziet,wall_time(time.time()-bigBang),
                                            len(df["nElectron"]),1000*filetime/len(df["nElectron"])))
     fs.close()
