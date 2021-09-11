962e8716b2f0d250305d4f6a1218b0a4eeb25242
Antra <79895853+antra-gaile@users.noreply.github.com>
2021-09-11 11:18:27 +0300

    Create Zee_data_17_money_plot.py
    Going for this https://indico.cern.ch/event/881539/contributions/3727624/attachments/1981161/3299285/ul2017_validationSuite_20200131_vF.pdf#page=23

---
+++ Zee_data_17_money_plot.py
@@ -0,0 +1,82 @@
+# Zee data, money plots
+# 2017. gadam
+
+import time
+bigBang=time.time()
+import uproot
+import ROOT
+import numpy as np
+import pandas as pn
+
+def wall_time(seconds):
+    m, s = divmod(seconds, 60)
+    h, m = divmod(m, 60)
+    return "{0:.0f}h:{1:.0f}min:{2:.0f}s".format(h,m,s) #lxplus nepatik d
+def l_wall_time(seconds):
+    m, s = divmod(seconds, 60)
+    return "{0:.0f}min:{1:.0f}s".format(m,s)
+
+names=pn.read_csv("all_nosaukumi_2017_data_v9.txt", sep='\t',names=["nos"]) 
+print("I have the libraries and names")
+
+notik=0
+tik=len(names["nos"])
+for aiziet in range(notik,tik):
+    print("\n {0}/{1}, {2}".format(aiziet+1,tik,wall_time(time.time()-bigBang)))
+    start=time.time()
+        
+    rootfile="root://cmsxrootd.fnal.gov///"+names["nos"][aiziet]
+    tree = uproot.open(rootfile)["Events"]
+    print(" {0}\tI have the tree".format(l_wall_time(time.time()-start)))
+
+    el_mass = []
+    subsection = ["Electron_pt","nElectron","Electron_eta",
+                  "Electron_charge","Electron_phi",
+                  "Electron_cutBased","Electron_r9"]
+    ds = tree.pandas.df(subsection, entrystop=-1)
+    print(" {0}\tI have the branches".format(l_wall_time(time.time()-start)))
+
+    df=ds.query("nElectron==2")
+    df=df.reset_index()
+    print(" {0}\tI have the diE, {1}".format(l_wall_time(time.time()-start),len(df["nElectron"])))
+    
+    for i in range(len(df["nElectron"])):
+        if df["Electron_r9"][i][0] > 0.96 and df["Electron_r9"][i][1] > 0.96:
+            if df["Electron_eta"].abs()[i][0] <1.479 and df["Electron_eta"].abs()[i][1] <1.479:
+                if df["Electron_cutBased"][i][0] in [2,3,4] and df["Electron_cutBased"][i][1]in [2,3,4]:
+                    if (df["Electron_charge"][i][0]+df["Electron_charge"][i][1] == 0):
+                        if df["Electron_pt"][i][0] > df["Electron_pt"][i][1]:
+                            if not (df["Electron_pt"][i][0] > 32 and df["Electron_pt"][i][1] > 20): 
+                                continue
+                        else:
+                            if not (df["Electron_pt"][i][0] > 20 and df["Electron_pt"][i][1] > 32): 
+                                continue
+                        
+                        el1 = ROOT.TLorentzVector()
+                        el2 = ROOT.TLorentzVector()
+
+                        el1.SetPtEtaPhiM(df["Electron_pt"][i][0],
+                                         df["Electron_eta"][i][0],
+                                         df["Electron_phi"][i][0],0.000511)
+                        el2.SetPtEtaPhiM(df["Electron_pt"][i][1],
+                                         df["Electron_eta"][i][1],
+                                         df["Electron_phi"][i][1],0.000511)     
+                        if (el1+el2).M()<80 or (el1+el2).M()>100:
+                            continue
+                        el_mass.append((el1+el2).M())
+
+    filetime=time.time()-start
+    print(" {0}\tI have the raw data".format(l_wall_time(filetime)))
+    print("  each {0:.2f} ms".format(1000*filetime/len(df["nElectron"])))
+    savetime=time.time()
+    
+    fo=open("2017_data_v9_money_plot_{0}_{1}.txt".format(notik,tik),"a")
+    for i in range(len(el_mass)):
+        fo.write(str(el_mass[i])+"\n")
+    fo.close()
+    
+    fs=open("2017_data_v9_money_plot_progress_{0}_{1}.txt".format(notik,tik),"a")
+    fs.write("{0}\t{1}\t{2}\t{3:.2f}\n".format(aiziet,wall_time(time.time()-bigBang),
+                                           len(df["nElectron"]),1000*filetime/len(df["nElectron"])))
+    fs.close()
+print("{0}\tAll done".format(wall_time(time.time()-bigBang)))   
